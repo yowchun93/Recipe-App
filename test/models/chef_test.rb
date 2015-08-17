b0d90ee4bc3cf38457b1a18e60489a00e40c051e
YcYap <yowchun93@gmail.com>
2015-08-17 22:41:35 +0800

    create chef and recipes association and tests

--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -4,4 +4,41 @@ class ChefTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+  	@chef = Chef.new(chefname: "john", email: "john@exameple.com")
+  end
+
+  test "chef should be valid" do
+  	assert @chef.valid?
+  end
+
+  test "chefname should be present" do 
+  	@chef.chefname = " " 
+  	assert_not @chef.valid?
+  end
+
+  test "email address should be unique" do
+  	dup_chef = @chef.dup 
+  	dup_chef.email = @chef.email.upcase 
+  	@chef.save
+  	assert_not dup_chef.valid? 
+  end
+
+  test "email validation should accept valid address" do 
+  	valid_addresses =  %w[user@eee.com R_TDD-DS@eee.hello.org user@example.com first.last@eem.au laura_joe@monk.cm]
+  	valid_addresses.each do |va| 
+  		@chef.email = va 
+  		assert @chef.valid?  , '#{va.inspect} should be valid '
+  	end
+  end
+
+  test "email validation should reject invalid address" do 
+  	invalid_addresses = %w[user@xample,com user_at_eee.org user.name@xample. eee@i_am.com foo@ee_aar.com]
+  	invalid_addresses.each do |invalid|
+  		@chef.email = invalid 
+  		assert_not @chef.valid?, '#{invalid.inspect} is invalid '
+   	end  
+  end
+
+
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -4,4 +4,66 @@ class RecipeTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-end
+
+  # testing is like running a sandbox
+  def setup
+  	@chef = Chef.create(chefname: "bob", email: "bob@example.com")
+  	@recipe = @chef.recipes.build(name: "chicken parm", summary: "this is the best chicken parm", 
+  			description: "heat oil, add onion,do something")
+  end
+
+  test "chef_id should be present" do 
+  	@recipe.chef_id = nil 
+  	assert_not @recipe.valid? 
+  end 
+
+  test "recipe should be valid" do
+  	assert @recipe.valid?
+  end
+
+  test "name should be present" do
+  	@recipe.name = " "
+  	assert_not @recipe.valid?  
+  end
+
+  test "name length should not be too long" do 
+  	@recipe.name = "a" * 101 
+  	assert_not @recipe.valid? 
+  end
+
+  test "name length should not be too short" do 
+  	@recipe.name = "aaaa"
+  	assert_not @recipe.valid? 
+  end
+
+  test "summary length should be present" do 
+  	@recipe.summary = " "
+  	assert_not @recipe.valid? 
+  end
+
+  test "summary length should not be too long" do
+  	@recipe.summary = "a" * 151 
+  	assert_not @recipe.valid?
+  end
+
+  test "summary length should not be too short" do
+  	@recipe.summary = "a" * 9 
+  	assert_not @recipe.valid? 
+  end
+
+  test "description must be present" do 
+  	@recipe.description = " "
+  	assert_not @recipe.valid? 
+  end
+
+  test "description should not be too long" do 
+  	@recipe.description = "a" * 501
+  	assert_not @recipe.valid?
+  end
+
+  test "description should not be too short" do
+  	@recipe.description = "a" * 19 
+  	assert_not @recipe.valid? 
+  end   
+
+ end
